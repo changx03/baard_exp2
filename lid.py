22698acc4af52618c455cf5e598c397f2ad0eec7
Luke Chang <luke.x.chang@gmail.com>
2020-12-14 00:55:36 +1300

    fixed bug in LID

--- lid.py
+++ lid.py
@@ -11,6 +11,7 @@ def mle_batch(data, batch, k):
     k = min(k, len(data)-1)
 
     def mle(v):
+        # v[-1] is the max of the neighbour distances
         return - k / np.sum(np.log(v/v[-1]))
 
     a = cdist(batch, data)
@@ -19,13 +20,11 @@ def mle_batch(data, batch, k):
     return a
 
 
-def get_lid_random_batch(sequence, X, X_noisy, X_adv, k, batch_size,):
+def get_lid_random_batch(sequence, X, X_noisy, X_adv, k, batch_size, device='cpu'):
     """
     Get the local intrinsic dimensionality of each Xi in X_adv
     estimated by k close neighbours in the random batch it lies in.
     """
-    # This part runs on CPU only
-    device = 'cpu'
     sequence.to(device)
 
     n_hidden_layers = len(list(sequence.children())) - 1
@@ -34,36 +33,38 @@ def get_lid_random_batch(sequence, X, X_noisy, X_adv, k, batch_size,):
     for i in range(1, n_hidden_layers+1):
         layer = nn.Sequential(*list(sequence.children())[:i])
         hidden_layers.append(layer)
-    # print('Number of hidden layers: {}'.format(len(hidden_layers)))
+    print('Number of hidden layers: {}'.format(len(hidden_layers)))
 
     # Convert numpy Array to PyTorch Tensor
-    X = torch.tensor(X, dtype=torch.float32)
-    X_noisy = torch.tensor(X_noisy, dtype=torch.float32)
-    X_adv = torch.tensor(X_adv, dtype=torch.float32)
+    X = torch.tensor(X, dtype=torch.float32).to(device)
+    X_noisy = torch.tensor(X_noisy, dtype=torch.float32).to(device)
+    X_adv = torch.tensor(X_adv, dtype=torch.float32).to(device)
 
     def estimate(i_batch):
         start = i_batch * batch_size
         end = np.minimum(len(X), (i_batch + 1) * batch_size)
         n_feed = end - start
-        lid_batch = np.zeros((n_feed, n_hidden_layers), dtype=np.float32)
+        lid_batch_benign = np.zeros(
+            (n_feed, n_hidden_layers), dtype=np.float32)
         lid_batch_noisy = np.zeros((n_feed, n_hidden_layers), dtype=np.float32)
         lid_batch_adv = np.zeros((n_feed, n_hidden_layers), dtype=np.float32)
 
         for i, layer in enumerate(hidden_layers):
             layer.eval()
-            output = layer(X[start: end])
-            output = output.cpu().detach().numpy().reshape((n_feed, -1))
+            batch_data = torch.cat(
+                (X[start: end], X_noisy[start: end], X_adv[start: end]))
+            output = layer(batch_data)
+            output = output.view(output.size(0), -1).cpu().detach().numpy()
 
-            output_noisy = layer(X_noisy[start: end])
-            output_noisy = output_noisy.cpu().detach().numpy().reshape((n_feed, -1))
+            output_benign = output[:n_feed]
+            output_noisy = output[n_feed: n_feed+n_feed]
+            output_adv = output[n_feed+n_feed:]
 
-            output_adv = layer(X_noisy[start: end])
-            output_adv = output_adv.cpu().detach().numpy().reshape((n_feed, -1))
-
-            lid_batch[:, i] = mle_batch(output, output, k=k)
-            lid_batch_noisy[:, i] = mle_batch(output, output_noisy, k=k)
-            lid_batch_adv[:, i] = mle_batch(output, output_adv, k=k)
-        return lid_batch, lid_batch_noisy, lid_batch_adv
+            lid_batch_benign[:, i] = mle_batch(
+                output_benign, output_benign, k=k)
+            lid_batch_noisy[:, i] = mle_batch(output_benign, output_noisy, k=k)
+            lid_batch_adv[:, i] = mle_batch(output_benign, output_adv, k=k)
+        return lid_batch_benign, lid_batch_noisy, lid_batch_adv
 
     lid_benign = []
     lid_adv = []
@@ -87,17 +88,17 @@ def merge_and_generate_labels(X_pos, X_neg):
     """
     X_pos = X_pos.reshape(X_pos.shape[0], -1)
     X_neg = X_neg.reshape(X_neg.shape[0], -1)
-    print(X_pos.shape, X_neg.shape)
+    # print(X_pos.shape, X_neg.shape)
     X = np.concatenate((X_pos, X_neg))
     y = np.concatenate((np.ones(X_pos.shape[0]), np.zeros(X_neg.shape[0])))
     y = y.astype(np.long)
     return X, y
 
 
-def get_lid(sequence, X, X_noisy, X_adv, k=20, batch_size=100):
+def get_lid(sequence, X, X_noisy, X_adv, k=20, batch_size=100, device='cpu'):
     """Get local intrinsic dimensionality (LID)"""
     lid_benign, lid_noisy, lid_adv = get_lid_random_batch(
-        sequence, X, X_noisy, X_adv, k, batch_size)
+        sequence, X, X_noisy, X_adv, k, batch_size, device)
     lid_pos = lid_adv
     lid_neg = np.concatenate((lid_benign, lid_noisy))
     artifacts, labels = merge_and_generate_labels(lid_pos, lid_neg)

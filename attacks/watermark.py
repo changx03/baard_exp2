97ddbdba4fe2e4619dd74c3f16b1cde8fbe71c03
Luke Chang <luke.x.chang@gmail.com>
2021-01-25 19:59:24 +1300

    removed comments

--- attacks/watermark.py
+++ attacks/watermark.py
@@ -4,6 +4,7 @@ This module implements the watermark attack.
 
 import numpy as np
 from sklearn.neighbors import BallTree
+from tqdm import trange
 
 
 class WaterMarkAttack:
@@ -13,13 +14,15 @@ class WaterMarkAttack:
                  n_classes=10,
                  x_min=0.0,
                  x_max=1.0,
-                 targeted=False):
+                 targeted=False,
+                 verbose=True):
         self.eps = eps
         self.base = base
         self.n_classes = n_classes
         self.x_min = x_min
         self.x_max = x_max
         self.targeted = targeted
+        self.verbose = verbose
 
         self.trees_ = []
         self.X_train_ = []
@@ -28,14 +31,14 @@ class WaterMarkAttack:
         X = np.reshape(X, (X.shape[0], -1))
         if self.targeted:
             # tree contains only targeted samples.
-            for i in range(self.n_classes):
+            for i in trange(self.n_classes, disable=not self.verbose):
                 idx = np.where(y == i)[0]
                 tree = BallTree(X[idx], leaf_size=32)
                 self.trees_.append(tree)
                 self.X_train_.append(X[idx])
         else:  # untargeted
             # tree contains samples other than the true label.
-            for i in range(self.n_classes):
+            for i in trange(self.n_classes, disable=not self.verbose):
                 idx = np.where(y != i)[0]
                 tree = BallTree(X[idx], leaf_size=128)
                 self.trees_.append(tree)
@@ -45,17 +48,14 @@ class WaterMarkAttack:
         shape = X.shape
         X = np.reshape(X, (X.shape[0], -1))
         X_adv = np.copy(X)
-        for i in range(self.n_classes):
+        for i in trange(self.n_classes, disable=not self.verbose):
             idx = np.where(y == i)[0]
             if len(idx) == 0:
                 continue
             idx_neigh = self.trees_[i].query(X[idx], k=1, return_distance=False)
             idx_neigh = np.squeeze(idx_neigh)
-#             print(self.X_train_[i].shape)
             X_neigh = self.X_train_[i][idx_neigh]
-#             print(X_neigh.shape)
             temp_adv = self.base * X[idx] + self.eps * X_neigh
-#             print(temp_adv.shape)
             temp_adv = np.clip(temp_adv, self.x_min, self.x_max)
             X_adv[idx] = temp_adv
         return np.reshape(X_adv, shape)

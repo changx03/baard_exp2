f4048fe182985554dd264e9ff7ffdecb869296ff
Luke Chang <luke.x.chang@gmail.com>
2021-01-26 03:37:47 +1300

    fixed signature of generate function

--- attacks/watermark.py
+++ attacks/watermark.py
@@ -48,18 +48,18 @@ class WaterMarkAttack:
                 self.trees_.append(tree)
                 self.X_train_.append(X[idx])
 
-    def generate(self, X, y):
-        shape = X.shape
-        X = np.reshape(X, (X.shape[0], -1))
-        X_adv = np.copy(X)
+    def generate(self, x, y):
+        shape = x.shape
+        x = np.reshape(x, (x.shape[0], -1))
+        X_adv = np.copy(x)
         for i in trange(self.n_classes, disable=not self.verbose):
             idx = np.where(y == i)[0]
             if len(idx) == 0:
                 continue
-            idx_neigh = self.trees_[i].query(X[idx], k=1, return_distance=False)
+            idx_neigh = self.trees_[i].query(x[idx], k=1, return_distance=False)
             idx_neigh = np.squeeze(idx_neigh)
             X_neigh = self.X_train_[i][idx_neigh]
-            temp_adv = self.base * X[idx] + self.eps * X_neigh
+            temp_adv = self.base * x[idx] + self.eps * X_neigh
             temp_adv = np.clip(temp_adv, self.x_min, self.x_max)
             X_adv[idx] = temp_adv
         return np.reshape(X_adv, shape)

e28cec1e9de69f06eb8015609b848d4644430d03
dlon450 <dlon450@aucklanduni.ac.nz>
2021-01-30 19:06:00 +1300

    added all tables

--- create_tables.py
+++ create_tables.py
@@ -13,30 +13,26 @@ for data in all_data:
 
     # table 1 (same as example)
     df1 = df.pivot_table(index=['Attack', 'Epsilon'],columns=['Defence'],values=['Score','False Positive Rate'])\
-        .rename(columns={"baard_2stage":"BAARD2","baard_3stage":"BAARD3","fs":"FS","lid":"LID","rc":"RC","Block Rate":"Blocking Rate","False Positive Rate":"FPR"})
+        .rename(columns={"baard_2stage":"BAARD2","baard_3stage":"BAARD3","fs":"FS","lid":"LID","rc":"RC","Score":"Success","False Positive Rate":"FPR"})
     df1.columns = df1.columns.swaplevel(0,1).rename(["",""])
     df1.sort_index(axis=1,level=0,inplace=True)
-    df1.drop('FPR',inplace=True,axis=1)
     save_path = os.path.join('tables',data+'_1.xlsx')
-    print(df1)
     df1.to_excel(save_path)
 
     # table 2 (without FPR)
-    df1 = df.pivot_table(index=['Attack', 'Epsilon'],columns=['Defence'],values=['Block Rate','False Positive Rate'])\
+    df2 = df.pivot_table(index=['Attack', 'Epsilon'],columns=['Defence'],values=['Block Rate','False Positive Rate'])\
         .rename(columns={"baard_2stage":"BAARD2","baard_3stage":"BAARD3","fs":"FS","lid":"LID","rc":"RC","Block Rate":"Blocking Rate","False Positive Rate":"FPR"})
-    df1.columns = df1.columns.swaplevel(0,1).rename(["",""])
-    df1.sort_index(axis=1,level=0,inplace=True)
+    df2.columns = df2.columns.swaplevel(0,1).rename(["",""])
+    df2.sort_index(axis=1,level=0,inplace=True)
 
-    save_path = os.path.join('tables',data+'_1.xlsx')
-    print(df1.columns)
-    df1.to_excel(save_path)
+    save_path = os.path.join('tables',data+'_2.xlsx')
+    df2.to_excel(save_path)
 
     # table 3 (swap with Blocking Rate)
-    df1 = df.pivot_table(index=['Attack', 'Epsilon'],columns=['Defence'],values=['Block Rate','False Positive Rate'])\
-        .rename(columns={"baard_2stage":"BAARD2","baard_3stage":"BAARD3","fs":"FS","lid":"LID","rc":"RC","Block Rate":"Blocking Rate","False Positive Rate":"FPR"})
-    df1.columns = df1.columns.swaplevel(0,1).rename(["",""])
-    df1.sort_index(axis=1,level=0,inplace=True)
+    df3 = df.pivot_table(index=['Attack', 'Epsilon'],columns=['Defence'],values=['Score'])\
+        .rename(columns={"baard_2stage":"BAARD2","baard_3stage":"BAARD3","fs":"FS","lid":"LID","rc":"RC","Score":"Success"})
+    df3.columns = df3.columns.swaplevel(0,1).rename(["",""])
+    df3.sort_index(axis=1,level=0,inplace=True)
 
-    save_path = os.path.join('tables',data+'_1.xlsx')
-    print(df1.columns)
-    df1.to_excel(save_path)
+    save_path = os.path.join('tables',data+'_3.xlsx')
+    df3.to_excel(save_path)

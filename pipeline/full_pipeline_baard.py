1e05c3f6bf94fa5d8be63da20dde5bc22363999f
Luke Chang <luke.x.chang@gmail.com>
2021-02-04 15:48:19 +1300

    Array size should not change!

--- pipeline/full_pipeline_baard.py
+++ pipeline/full_pipeline_baard.py
@@ -88,7 +88,7 @@ def run_full_pipeline_baard(data,
 
     print('-------------------------------------------------------------------')
     print('Start testing adversarial examples...')
-    pred = predict_numpy(model, adv[:1000], device)
+    pred = predict_numpy(model, adv, device)
     print('Acc on adv:', np.mean(pred == y[:1000]))
 
     X_def_test = X[:1000]

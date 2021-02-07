378ed6a162482ccac4496003ff204edfa3a9b1d3
ambra <ambra.demontis@diee.unica.it>
2021-02-07 13:18:56 +0100

    change the num of samples used (changed in the previous commit)

--- pipeline/run_whitebox_attack_against_baard.py
+++ pipeline/run_whitebox_attack_against_baard.py
@@ -170,8 +170,8 @@ def main(seed, dataset_name, clf_name, detector_name, epsilon_lst,input_shape):
                                                        clf_name, detector_name)
 
     print("compute prediction for samples at epsilon 0")
-    x = X_att_test[:10]
-    y = y_att_test[:10]
+    x = X_att_test[:1000]
+    y = y_att_test[:1000]
 
     # compute and save predictions
     cmpt_and_save_predictions(model, art_detector, detector, device, x, y,

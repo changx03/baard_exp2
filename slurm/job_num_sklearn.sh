1c561dec4a14bf54d41c8d523c4a0bff33903a16
Luke Chang <luke.x.chang@gmail.com>
2021-03-03 22:16:54 +1300

    run 10 repeatation on sklearn

--- slurm/job_num_sklearn.sh
+++ slurm/job_num_sklearn.sh
@@ -4,7 +4,7 @@
 #SBATCH --time=12:00:00
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=4G
-#SBATCH --array=0-4
+#SBATCH --array=0-9
 
 module load Python/3.7.3-gimkl-2018b
 source /nesi/project/uoa02977/baard_exp2/venv/bin/activate
